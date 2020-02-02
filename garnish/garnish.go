c6d704a50037d7a7a4c38a67e8e2131794130f8b
Bartłomiej Kiełbasa <bartlomiej.kielbasa@brainly.com>
2020-02-02 11:34:46 +0100

    refactor the ServeHTTP func

--- garnish/garnish.go
+++ garnish/garnish.go
@@ -35,22 +35,21 @@ func (g *garnish) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 
 	u := r.URL.String()
 	cached := g.c.get(u)
-	if cached == nil {
-		proxyRW := &responseWriter{
-			proxied: rw,
-		}
-		proxyRW.Header().Set(Xcache, XcacheMiss)
-		g.proxy.ServeHTTP(proxyRW, r)
-
-		cc := rw.Header().Get(cacheControl)
-		toCache, duration := parseCacheControl(cc)
-		if toCache {
-			g.c.store(u, proxyRW.body, duration)
-		}
-
-		return
-	} else {
+	if cached != nil {
 		rw.Header().Set(Xcache, XcacheHit)
 		_, _ = rw.Write(cached)
+		return
+	}
+
+	proxyRW := &responseWriter{
+		proxied: rw,
+	}
+	proxyRW.Header().Set(Xcache, XcacheMiss)
+	g.proxy.ServeHTTP(proxyRW, r)
+
+	cc := rw.Header().Get(cacheControl)
+	toCache, duration := parseCacheControl(cc)
+	if toCache {
+		g.c.store(u, proxyRW.body, duration)
 	}
 }
