fa0006d2cfa8eeb4e0f2d7e366b3372b437a670a
于欣然 <DELL@MacBook-Pro-145.local>
2022-06-30 08:16:57 -0400

    comments to understand garnish implementation

--- garnish/garnish.go
+++ garnish/garnish.go
@@ -15,16 +15,18 @@ type garnish struct {
 	proxy *httputil.ReverseProxy
 }
 
+// process: requested url --> garnish
 func New(url url.URL) *garnish {
 	director := func(req *http.Request) {
 		req.URL.Scheme = url.Scheme
 		req.URL.Host = url.Host
 	}
-
+	// director to modify the request
 	reverseProxy := &httputil.ReverseProxy{Director: director}
 	return &garnish{c: newCache(), proxy: reverseProxy}
 }
 
+// create a handler object--> once server get the request, it will handle the request and construct response
 func (g *garnish) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	// only GET requests should be cached
 	if r.Method != http.MethodGet {
@@ -35,12 +37,12 @@ func (g *garnish) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 
 	u := r.URL.String()
 	cached := g.c.get(u)
-	if cached != nil {
+	if cached != nil { // handle Xcache -response
 		rw.Header().Set(Xcache, XcacheHit)
 		_, _ = rw.Write(cached)
 		return
 	}
-
+	//instantiate the interface(struct responseWriter)
 	proxyRW := &responseWriter{
 		proxied: rw,
 	}
